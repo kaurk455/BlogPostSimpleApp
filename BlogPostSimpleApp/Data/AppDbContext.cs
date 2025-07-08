6350ae43e1e53f0f08b5f42592cd05657698d1ee
studentavd <studentavd@ML-RefVm-842662>
2025-07-07 18:05:18 -0600

    Add project files.

---
+++ BlogPostSimpleApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlogPostSimpleApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+ => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BlogDb;Trusted_Connection = True;");
+
+
+}
---
+++ BlogPostSimpleApp/Models/BlogType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPostSimpleApp.Models
+
+    {
+        public class BlogType
+        {
+            public int BlogTypeId { get; set; }
+            public int Status { get; set; }     
+            public string Name { get; set; }    
+            public string Description { get; set; } 
+        }
+    }
+
+
